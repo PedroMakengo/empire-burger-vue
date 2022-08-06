280aef6808e9f2cbfc3a10102ec06ae5f40d7c51
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-06 09:04:31 +0100

    Trabalhando na arquitectura

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Ola Mundo</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import { books } from '@/store'
+
+export default Vue.extend({
+  layout: 'ibook',
+})
+</script>
