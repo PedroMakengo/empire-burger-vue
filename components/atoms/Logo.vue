a854ae1d5d6222b46e68d29f773f1846dff8a2d3
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-07 12:36:23 +0100

    Montando os componentens

---
+++ components/atoms/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLink to="/" class="logo">
+    <img src="@/assets/img/logo.svg" alt="" />
+    <h2>Empire <span>Burger</span></h2>
+  </NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  img {
+    width: 100%;
+  }
+  h2 {
+    font-weight: bold;
+    text-transform: uppercase;
+    color: rgba(29, 6, 5, 0.9);
+    span {
+      font-weight: 300;
+    }
+  }
+}
+</style>
---
+++ components/molecules/Navegator.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navegator">
+    <ul>
+      <li v-for="link of links" :key="link.id">
+        <NuxtLink :to="link.to">{{ link.text }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      links: [
+        { id: 1, to: '/', text: 'Home' },
+        { id: 2, to: '/', text: 'Promoção' },
+        { id: 3, to: '/', text: 'Cardápio' },
+        { id: 4, to: '/', text: 'Comentário' },
+        { id: 5, to: '/', text: 'Contato' },
+      ],
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.navegator {
+  ul {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+
+    li {
+      a {
+        text-decoration: none;
+        color: color('text', 'title-87');
+      }
+    }
+  }
+}
+</style>
