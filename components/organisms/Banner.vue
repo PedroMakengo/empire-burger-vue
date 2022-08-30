9a810c4a8722fea8104b11fb7604913f08ba8d95
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-30 14:58:50 -0300

    Adicionando a parte do banner

--- components/organisms/Banner.vue
+++ components/organisms/Banner.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="banner"></div>
+  <div class="banner">
+    <div class="banner-content">
+      <span>Uma nova experiência!</span>
+      <h2>King <span>Burguer</span></h2>
+      <p>Para quem tem um <span>Apetite de um Rei</span></p>
+      <a href="#">Comprar agora</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
